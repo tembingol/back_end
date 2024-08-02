0483d2d6aed7389a350942e6d14b415ecefaea36
tembingol <tembingol@ya.ru>
2024-08-02 20:49:59 +0500

    add: added 2 solved tasks
    Signed-off-by: tembingol <tembingol@ya.ru>

--- codewars_tasks_day_4.js
+++ codewars_tasks_day_4.js
@@ -30,3 +30,42 @@ function reverseWords(str) {
   return result.join(" ");
 }
 //task 3 ---
+
+//task 4 +++
+/*
+8 kyu
+A Needle in the Haystack
+*/
+function findNeedle(haystack) {
+  // your code here
+  let index = haystack.indexOf("needle");
+  if (index > -1) {
+    return "found the needle at position " + index;
+  }
+}
+//task 4 ---
+
+//task 5+++
+/*
+8 kyu
+Count of positives / sum of negatives
+*/
+function countPositivesSumNegatives(input) {
+  if (input === null || input.length == 0) {
+    return [];
+  }
+
+  let count = 0;
+  let sum = 0;
+
+  input.forEach(function (value, index) {
+    if (value > 0) {
+      count += 1;
+    } else if (value < 0) {
+      sum += value;
+    }
+  });
+
+  return [count, sum];
+}
+//task 5 ---
